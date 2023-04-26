2bc207d60bd92d27dc0542c5e02f193dc6f5f8d6
lukas licon <lukas@licons.com>
2023-04-26 15:51:05 -0700

    added working first scene

--- mainscript.js
+++ mainscript.js
@@ -1,16 +1,46 @@
-class JimmyGame extends Phaser.Scene{
-    constructor(){
-        super("jimmyGame");
+//first scene, studio introduction
+class SceneA extends Phaser.Scene{
+    constructor()
+    {
+        super({key:"sceneA"});
     }
-
+    preload (){       
+    this.load.image('studioLogo', 'assets/buggyGames.png');}
+    create (){
+        this.cameras.main.fadeIn();
+        this.add.sprite(400, 300, 'studioLogo').setAlpha(0.2);
+        this.time.delayedCall(4000, () => {
+        this.cameras.main.fadeOut();    
+    }, this);
+        this.time.delayedCall(5000, () => {
+            this.scene.start('sceneB');    
+    }, this);
+}
 }
 
-let config = {
+//second scene, tired of bugs scene
+class SceneB extends Phaser.Scene{
+    constructor()
+    {
+        super("sceneB");
+    }
+    preload (){}
+    create (){
+        this.cameras.main.fadeIn();
+
+    }
+}
+
+//third scene, explanation scene
+
+//fourth scene, menu scene, goes back to studio name
+
+config = {
     type: Phaser.WEBGL,
     width: 800,
     height: 600, 
-    backgroundColor: 0xA62FCF,
-    scene: [JimmyGame],
+    backgroundColor: 0x000000,
+    scene: [SceneA, SceneB],
 }
 
 let game = new Phaser.Game(config);
