e78b6488fa1a03fdd63edf397455c964f703b34b
Lukas Licon <lukas@licons.com>
2023-04-26 22:18:02 -0700

    scene A done

--- mainscript.js
+++ mainscript.js
@@ -1,19 +1,25 @@
-//first scene, studio introduction
+//first scene, studio introduction //done
 class SceneA extends Phaser.Scene{
     constructor()
     {
-        super({key:"sceneA"});
+        super("SceneA");
     }
     preload (){       
-    this.load.image('studioLogo', 'assets/buggyGames.png');}
+    this.load.image('logo', 'assets/BuggyGames.png');}
     create (){
         this.cameras.main.fadeIn();
-        this.add.sprite(400, 300, 'studioLogo').setAlpha(0.2);
+
+        this.imageObject = this.add.image(
+            400,//x
+            250,//y
+            'logo',
+        )
+        //next scene
         this.time.delayedCall(4000, () => {
         this.cameras.main.fadeOut();    
     }, this);
         this.time.delayedCall(5000, () => {
-            this.scene.start('sceneB');    
+            this.scene.start('SceneB');    
     }, this);
 }
 }
@@ -22,25 +28,113 @@ class SceneA extends Phaser.Scene{
 class SceneB extends Phaser.Scene{
     constructor()
     {
-        super("sceneB");
+        super("SceneB");
     }
-    preload (){}
+    preload (){
+        this.load.image('grass', 'assets/grass.png');
+        this.load.audio('music', 'assets/audio/music.mp3');
+    }
     create (){
         this.cameras.main.fadeIn();
+        this.imageObject = this.add.image(
+            400,//x
+            250,//y
+            'grass',
+        )
+        this.imageObject.setScale(1.25) //resize
+        this.music = this.sound.add('music');
 
+        let musicConfig = {
+            mute:false,
+            volume: 1,
+            rate: 1,
+            detune: 0,
+            seek: 0,
+            loop: true,
+            delay: 0
+        }
+        this.music.play(musicConfig); //play music
+
+        //next scene
+        this.time.delayedCall(4000, () => {
+            this.cameras.main.fadeOut();    
+        }, this);
+            this.time.delayedCall(5000, () => {
+                this.scene.start('SceneC');    
+        }, this);
     }
 }
 
 //third scene, explanation scene
+class SceneC extends Phaser.Scene{
+    constructor()
+    {
+        super("SceneC");
+    }
+    preload (){
+    this.load.image('grass', 'assets/grass.png');       
+    }
+    create (){
+        
+        this.cameras.main.fadeIn();
+        this.imageObject = this.add.image(
+            400,//x
+            250,//y
+            'grass',
+        )
+        this.imageObject.setScale(1.25) //resize
+
+        //next scene
+        this.time.delayedCall(4000, () => {
+            this.cameras.main.fadeOut();    
+        }, this);
+            this.time.delayedCall(5000, () => {
+                this.scene.start('SceneD');    
+        }, this);
+}
+}
 
 //fourth scene, menu scene, goes back to studio name
+class SceneD extends Phaser.Scene{
+    constructor()
+    {
+        super("SceneD");
+    }
+    preload (){
+    this.load.image('grass', 'assets/grass.png');       
+    this.load.audio('musicS', 'assets/audio/musicSped.wav');
+    }
+    create (){
+        this.game.sound.stopAll(); //stop current music
+        this.music = this.sound.add('musicS'); //play new music
+        let musicConfig = {
+            mute:false,
+            volume: 1,
+            rate: 1,
+            detune: 0,
+            seek: 0,
+            loop: true,
+            delay: 0
+        }
+        this.music.play(musicConfig); //play music
+        this.cameras.main.fadeIn();
+        this.imageObject = this.add.image(
+            400,//x
+            250,//y
+            'grass',
+        )
+        this.imageObject.setScale(1.25) //resize
+     
+}
+}
 
 config = {
-    type: Phaser.WEBGL,
+    type: Phaser.AUTO,
     width: 800,
     height: 600, 
-    backgroundColor: 0x000000,
-    scene: [SceneA, SceneB],
+    backgroundColor: 0xFFFFFF,
+    scene: [SceneA, SceneB, SceneC, SceneD],
 }
 
 let game = new Phaser.Game(config);
+
